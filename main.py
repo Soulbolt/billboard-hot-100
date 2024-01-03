aec598aff626d0a53c2a5be3004413eb47b0e169
Soulbolt <soulbolt@gmail.com>
2024-01-03 01:49:18 -0700

    install spotipy library, implemented methods to authenticate through spotify web api, saved client id, secret, username, token on .env and token file and added to .gitignore. implemented code to get user id  and query spotify web api for eachc song name and year. catch if song is not available on spotify and create a playlist to spotify account with given list scrapped  from billboard 100.

--- main.py
+++ main.py
@@ -1,14 +1,61 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
-user_date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:\n")
+load_dotenv()
+CLIENT_ID = os.getenv("SPOTIFY_CID")
+SECRET = os.getenv("SPOTIFY_SECRET")
+USER_NAME = os.getenv("USER_NAME")
+AUTH_ENDPOINT = "https://accounts.spotify.com/authorize?"
+SCOPE = "playlist-modify-private"
+# auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=SECRET)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    scope="playlist-modify-private",
+    redirect_uri="http://example.com",
+    client_id=CLIENT_ID,
+    client_secret=SECRET,
+    show_dialog=True,
+    cache_path="token.txt"
+))
+user_id = sp.user(USER_NAME)["id"]
+print(user_id)
 
-response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{user_date}")
-response.raise_for_status
-content = response.text
+# params = {
+#     "response_type": "code",
+#     "client_id": CLIENT_ID,
+#     "scope": SCOPE,
+#     "redirect_uri": "http://example.com",
+#     "state": "asdjkqitowktleof",
+# }
 
-soup = BeautifulSoup(content, "html.parser")
+# response = requests.get(url=AUTH_ENDPOINT, params=params)
+# response.raise_for_status
+# print(response.text)
 
-song_title_list = soup.select("div ul li ul h3")
-song_title = [song.text.strip() for song in song_title_list]
-print(f"song: {song_title}")
+# user_date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:\n")
+
+# response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{user_date}")
+# response.raise_for_status
+# content = response.text
+
+# soup = BeautifulSoup(content, "html.parser")
+
+# song_title_list = soup.select("div ul li ul h3")
+# song_title_names = [song.text.strip() for song in song_title_list]
+# song_uris = []
+
+# year = user_date.split("-")[0]
+# for song in song_title_names:
+#     result = sp.search(q=f"track:{song} year:{year}", type="track")
+#     # print(result)
+#     try:
+#         uri = result["tracks"]["items"][0]["uri"]
+#         song_uris.append(uri)
+#     except IndexError:
+#         print(f"{song} doesn't exist in Spotify. Skipped")
+
+# playlist = sp.user_playlist_create(user=user_id, name=f"{user_date} BIllboard 100", public=False)
+# sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
